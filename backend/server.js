663b2bc3939f83742e21ab7334d1a8977d263b7a
Ayman-Ahmed-23 <ayman507032@gmail.com>
2026-03-04 00:41:15 +0530

    deployable

--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,7 @@ dotenv.config();
 const PORT =process.env.PORT;
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL, // recommended
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/v1/auth', authRouter);
